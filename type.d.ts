cce7b27c9369664e92718a23c21e23762df4232a
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-12-21 08:52:27 +0100

    feat: more export helper

--- type.d.ts
+++ type.d.ts
@@ -2,3 +2,30 @@ export type virtualizeHandler = <V extends object, T extends object>(
   target: T,
   handler: ProxyHandler<T>,
 ) => ProxyHandler<V>;
+
+export type VirtualProxy = <V extends object, T extends object>(
+  virtual: V,
+  target: T,
+  handler: ProxyHandler<T>,
+) => V;
+
+export type RevocableVirtualProxy = <V extends object, T extends object>(
+  virtual: V,
+  target: T,
+  handler: ProxyHandler<T>,
+) => { revoke: () => void; proxy: V };
+
+export type VirtualObject = <V extends object, T extends object>(
+  target: T,
+  handler: ProxyHandler<T>,
+) => V;
+
+export type VirtualArray = <V extends any[], T extends object>(
+  target: T,
+  handler: ProxyHandler<T>,
+) => V;
+
+export type VirtualFunction = <V extends Function, T extends Function>(
+  target: T,
+  handler: ProxyHandler<T>,
+) => V;
